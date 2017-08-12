cac65e7f15328cd52484ec95f499442e93d5cf4a
Alex Rocha <alex.rochas@yahoo.com.br>
2017-08-11 21:43:37 -0300

    feat: cors working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,24 +4,19 @@ document.addEventListener('DOMContentLoaded', function(){
     event.preventDefault();
     var body = document.getElementById('url-input').value;
 
-    axios.get(body)
-        .then(function (response) {
-          console.log(response);
-          var tweetHtml = $(response.data).find(".tweet.permalink-tweet").prop('outerHTML');
-          window.goForIt = response;
-          console.log(tweetHtml);
-          var tweet = document.createElement('div');
-          tweet.classList.add('tweet');
-          tweet.style.width = '640px';
-          tweet.innerHTML = tweetHtml;
-          document.body.appendChild(tweet);
-          html2canvas(document.querySelector('.tweet'),{useCORS:true}).then(function(canvas) {
-            document.body.appendChild(canvas);
-          });
-
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    console.log(body);
+    $.getJSON('http://anyorigin.com/go?url=https%3A//twitter.com/adamatti/status/895762987679715328&callback=?', function(data){
+      var response = data.contents;
+      var tweetHtml = $(response).find(".tweet.permalink-tweet").prop('outerHTML');
+      window.goForIt = response;
+      var tweet = document.createElement('div');
+      tweet.classList.add('tweet');
+      tweet.style.width = '640px';
+      tweet.innerHTML = tweetHtml;
+      document.body.appendChild(tweet);
+      html2canvas(document.querySelector('.tweet'),{useCORS:true}).then(function(canvas) {
+        document.body.appendChild(canvas);
+      });
+    });
   }
 }, false);
